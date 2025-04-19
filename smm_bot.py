ef91408913e6661aabef1a89d13ffff42bc6fe6b
Tarun00888 <officialtarun162@gmail.com>
2025-04-19 16:59:24 +0530

    smm_bot.py

--- smm_bot.py
+++ smm_bot.py
@@ -3,16 +3,16 @@ import telebot
 import requests
 import os
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
-SMM_API_KEY = os.environ.get("SMM_API_KEY")
+BOT_TOKEN = os.environ.get("8053589642:AAFM2g0rd-lWwX0wmCf-N-mNWr9wFKuvvFk")
+SMM_API_KEY = os.environ.get("b028e8faf58eef167a0447cbe1a35de2c4becaba")
 CHAT_ID = '5335667019'
 
-bot = telebot.TeleBot(BOT_TOKEN)
+bot = telebot.TeleBot(8053589642:AAFM2g0rd-lWwX0wmCf-N-mNWr9wFKuvvFk)
 
 def get_balance():
     url = 'https://smmzz.com/api/v2'
     data = {
-        'key': SMM_API_KEY,
+        'key': b028e8faf58eef167a0447cbe1a35de2c4becaba ,
         'action': 'balance'
     }
     response = requests.post(url, data=data).json()
